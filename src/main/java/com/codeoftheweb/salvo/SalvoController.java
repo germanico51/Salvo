dbbffd890ea6f80dc49bfa34f5ab7f225a6d052f
germanico51 <germanicovilla@hotmail.com>
2019-10-02 12:31:36 -0300

    task incompleta

--- src/main/java/com/codeoftheweb/salvo/SalvoController.java
+++ src/main/java/com/codeoftheweb/salvo/SalvoController.java
@@ -2,9 +2,10 @@ package com.codeoftheweb.salvo;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
 
@@ -15,15 +16,31 @@ import static java.util.stream.Collectors.toSet;
 @RequestMapping("/api")
 public class SalvoController {
 
+    PasswordEncoder passwordEncoder;
+
     @Autowired
     private GameRepository gameRepository;
 
     @Autowired
     private GamePlayerRepository gamePlayerRepository;
-
+    
     @Autowired
     private PlayerRepository playerRepository;
 
+ @RequestMapping(path = '/players' , method = RequestMethod.POST)
+ public ResponseEntity<Object> register(
+     @RequestParam String email, @RequestParam String password){
+     if (email.isEmpty() || password.isEmpty() ) {
+         return new ResponseEntity<>("Missing data", HttpStatus.FORBIDDEN);
+     }
+
+     if (playerRepository.findByUserName(email) !=  null) {
+         return new ResponseEntity<>("Name already in use", HttpStatus.FORBIDDEN);
+     }
+
+     playerRepository.save(new Player(email, passwordEncoder.encode(password)));
+     return new ResponseEntity<>(HttpStatus.CREATED);
+ }
 
     @RequestMapping("/games")
     public List<Map<String, Object>> getGames(){
@@ -68,7 +85,6 @@ public class SalvoController {
                 .stream()
                 .map(Ship::getDto)
                 .collect(toList());
-
     }
 
 
