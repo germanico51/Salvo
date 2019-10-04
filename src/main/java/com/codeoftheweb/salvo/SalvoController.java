f3d3c58acad3d15f1ef0b4d6d4c37a69ffe1fa47
German Villalba <germanicovilla@hotmail.com>
2019-10-04 08:53:01 -0300

    task5.4

--- src/main/java/com/codeoftheweb/salvo/SalvoController.java
+++ src/main/java/com/codeoftheweb/salvo/SalvoController.java
@@ -1,9 +1,12 @@
 package com.codeoftheweb.salvo;
 
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,8 +28,9 @@ public class SalvoController {
 
     @Autowired
     private PlayerRepository playerRepository;
-/*
-  @RequestMapping(path = '/players' , method = RequestMethod.POST)
+
+
+  @RequestMapping(path = "/players", method = RequestMethod.POST)
  public ResponseEntity<Object> register(
      @RequestParam String email, @RequestParam String password){
      if (email.isEmpty() || password.isEmpty() ) {
@@ -37,13 +41,28 @@ public class SalvoController {
          return new ResponseEntity<>("Name already in use", HttpStatus.FORBIDDEN);
      }
 
-     playerRepository.save(new Player(email, passwordEncoder.encode(password)));
+     playerRepository.save(new Player(email, password));
      return new ResponseEntity<>(HttpStatus.CREATED);
- }*/
+ }
 
     @RequestMapping("/games")
-    public List<Map<String, Object>> getGames(){
-        return gameRepository.findAll().stream().map(Game -> Game.getDto()).collect(toList());
+    public Map<String, Object> getGames(Authentication authentication){
+        Map<String,Object> dto = new LinkedHashMap<>();
+        if (isGuest(authentication)){
+            dto.put("player", "Guest");
+        }else{
+            Player player = playerRepository.findByUserName(authentication.getName());
+            dto.put("player", player.getDto());
+        }
+        dto.put("games", gameRepository.findAll()
+                .stream()
+                .map(Game::getDto)
+                .collect(toList()));
+        return dto;
+    }
+
+    private boolean isGuest(Authentication authentication) {
+        return authentication == null || authentication instanceof AnonymousAuthenticationToken;
     }
 
     @RequestMapping("/game_view/{gamePlayerId}")
